c9dbe195e9e24b1da1c660961ce9cd3c092a43fc
Chau (Joe) Nguyen <chauey@hotmail.com>
2018-09-07 15:41:54 +0700

    update bootstrap, add angular material

--- Src/Assessment.Client/ClientApp/src/app/app.module.ts
+++ Src/Assessment.Client/ClientApp/src/app/app.module.ts
@@ -6,21 +6,20 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SharedComponentsModule } from './shared/shared.components.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' }
-    ])
+    ]),
+    SharedComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
